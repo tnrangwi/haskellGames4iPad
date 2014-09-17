70df3cdd46ddf707c9c505b112455126025c9c81
Toaster <trtoaster@googlemail.com>
2014-09-17 21:56:01 +0200

    roll function for labyrinth "3D"

--- Tools.hs
+++ Tools.hs
@@ -1,7 +1,16 @@
-module Tools where
+module Tools
 
-import System.Time
-import System.Random
+(
+  updateAt,
+  updateMap,
+  getRandomSequence,
+  roll
+)
+
+where
+
+import qualified System.Time as T
+import qualified System.Random as R
 
 -- |Update a sequence at the given position with the
 -- given element. Return original if index is out of
@@ -12,7 +21,7 @@ updateAt l i c = if or [i < 0, i >= length l] then
                  else
                      take i l ++ [c] ++ drop (i + 1) l
 
--- |Update a 2-dimensional map given as kist of strings
+-- |Update a 2-dimensional map given as list of strings
 -- with the given character at the given x / y 
 -- coordinates.
 updateMap :: Char -> [String] -> Int -> Int -> [String]
@@ -23,17 +32,16 @@ updateMap c m x y = if or [y < 0, x < 0, y >= length m, x >= length (m !! y)] th
 
 -- |Get a random sequence of given length, using the
 -- given std random number generator. All numbers from
--- 1 untik the given integer occur exactly once.
--- This may use
--- and arbitrary number of random numbers from the
+-- 1 until the given integer occur exactly once.
+-- This may use an arbitrary number of random numbers from the
 -- generator until the list is complete.
-randomUniqueSequence :: Int -> StdGen -> [Int]
+randomUniqueSequence :: Int -> R.StdGen -> [Int]
 randomUniqueSequence x g = gen [] g
         where
             gen l g = if length l == x then
                           l
                       else
-                           let (q, g1) = randomR (1, x) g
+                           let (q, g1) = R.randomR (1, x) g
                            in
                                if q `elem` l then
                                    gen l g1
@@ -45,7 +53,16 @@ randomUniqueSequence x g = gen [] g
 -- Current clocktime seconds are used to initialize
 -- the random number generator. This is suitable enough
 -- when a game just needs once such a series at startup.
+-- It is not, when sequences are needes quite often. In
+-- that case better fetch the random number generator and
+-- usw pure code to generate a large number of randoms.
 getRandomSequence :: Int -> IO [Int]
 getRandomSequence i =
-  getClockTime >>= return . randomUniqueSequence i . mkStdGen . ctSec . toUTCTime
+  T.getClockTime >>= return . randomUniqueSequence i . R.mkStdGen . T.ctSec . T.toUTCTime
+-- |Roll a finite enumeration to the next value -- switching to the first when the last
+-- value of the enumeration is reached.
+roll :: (Enum a, Bounded a, Eq a) => a -> a
+roll s | s == maxBound = toEnum 0
+       | otherwise = succ s
+
 
