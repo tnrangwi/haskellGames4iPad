670d374b9febad3793f5588cac4f2cf3f745cae0
Toaster <trtoaster@googlemail.com>
2015-01-06 22:11:21 +0100

    Make local settings a bit more flexible

--- LocalSettings.hs
+++ LocalSettings.hs
@@ -1,10 +1,53 @@
-module LocalSettings where
+module LocalSettings
+
+  (
+    screenSize
+  , screenWidth
+  , screenDetail
+  )
+
+where
+
+data DeviceType = Console | Ipad2 | Iphone4
+
+device :: DeviceType
 
 screenSize :: Int
-screenSize = screenSizeiPad2Landscape
+screenSize = case device of
+                 Console -> screenSizeConsole
+                 Ipad2 -> screenSizeiPad2Landscape
+                 Iphone4 -> screenSizeiPhone4Portrait
+
+screenSizeConsole :: Int
+screenSizeConsole = 25
 
 screenSizeiPad2Landscape :: Int
 screenSizeiPad2Landscape = 18
 
 screenSizeiPhone4Portrait :: Int
 screenSizeiPhone4Portrait = 9
+
+screenWidth :: Int
+screenWidth = case device of
+                  Console -> screenWidthConsole
+                  Ipad2 -> screenWidthiPad2Landscape
+                  Iphone4 -> screenWidthiPhone4Portrait
+
+screenWidthConsole :: Int
+screenWidthConsole = 80
+
+screenWidthiPad2Landscape :: Int
+screenWidthiPad2Landscape = 58
+
+screenWidthiPhone4Portrait :: Int
+screenWidthiPhone4Portrait = 20
+
+screenDetail :: Bool
+screenDetail = case device of
+                   Console -> True
+                   Ipad2 -> True
+                   Iphone4 -> False
+
+
+-- Configuration goes here
+device = Ipad2
