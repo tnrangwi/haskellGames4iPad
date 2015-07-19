5ec1678f89f6477e156dd5bece095e9033291304
Toaster <trtoaster@googlemail.com>
2015-07-19 17:49:30 +0200

    Add screen size values for iPhone6 (small)

--- LocalSettings.hs
+++ LocalSettings.hs
@@ -8,7 +8,7 @@ module LocalSettings
 
 where
 
-data DeviceType = Console | Ipad2 | Iphone4
+data DeviceType = Console | Ipad2 | Iphone4 | Iphone6Std
 
 device :: DeviceType
 
@@ -17,6 +17,7 @@ screenSize = case device of
                  Console -> screenSizeConsole
                  Ipad2 -> screenSizeiPad2Landscape
                  Iphone4 -> screenSizeiPhone4Portrait
+                 Iphone6Std -> screenSizeiPhone6StdPortrait
 
 screenSizeConsole :: Int
 screenSizeConsole = 25
@@ -27,11 +28,15 @@ screenSizeiPad2Landscape = 18
 screenSizeiPhone4Portrait :: Int
 screenSizeiPhone4Portrait = 9
 
+screenSizeiPhone6StdPortrait :: Int
+screenSizeiPhone6StdPortrait = 13
+
 screenWidth :: Int
 screenWidth = case device of
                   Console -> screenWidthConsole
                   Ipad2 -> screenWidthiPad2Landscape
                   Iphone4 -> screenWidthiPhone4Portrait
+                  Iphone6Std -> screenWidthiPhone6StdPortrait
 
 screenWidthConsole :: Int
 screenWidthConsole = 80
@@ -42,12 +47,16 @@ screenWidthiPad2Landscape = 58
 screenWidthiPhone4Portrait :: Int
 screenWidthiPhone4Portrait = 20
 
+screenWidthiPhone6StdPortrait :: Int
+screenWidthiPhone6StdPortrait = 35
+
 screenDetail :: Bool
 screenDetail = case device of
-                   Console -> True
-                   Ipad2 -> True
-                   Iphone4 -> False
+                Console -> True
+                Ipad2 -> True
+                Iphone4 -> False
+                Iphone6Std -> True
 
 
 -- Configuration goes here
-device = Ipad2
+device = Iphone6Std
